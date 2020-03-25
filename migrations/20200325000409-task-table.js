e4dbd003a3745ac2f8779c1887e1dee1bb0dc8b9
rnachison <rnachison@gmail.com>
2020-03-24 17:08:57 -0700

    deploy to heroku

---
+++ migrations/20200325000409-task-table.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isComplete: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      dueDate: {
+        type: Sequelize.DATE
+      },
+      notes: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId'
+        },
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tasks');
+  }
+};
