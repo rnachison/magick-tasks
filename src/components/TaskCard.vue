2f09ec355223ef7397e6765b7c99a712de508f42
rnachison <rnachison@gmail.com>
2020-04-19 11:44:41 -0700

    add moth and big ouroboros

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -48,7 +48,7 @@ export default {
   },
   data () {
     return {
-      symbols: ['demon-bottle', 'dragon-bottle', 'eyeball', 'ouroboros', 'moon', 'skull', 'sun']
+      symbols: ['demon-bottle', 'dragon-bottle', 'eyeball', 'infinite-ouroboros', 'moon', 'moth', 'ouroboros', 'skull', 'sun']
     }
   },
   components: {
